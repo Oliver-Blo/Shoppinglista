2b2c337fb4c9e7b3239050b40de4aad31064727a
DESKTOP-EH9DT1H\Oliver <47489@gymnasium.ax>
2023-05-17 13:57:28 +0300

    Uppdatera vara och första testerna i Postman fungerar

---
+++ Shoppinglista.js
@@ -0,0 +1,81 @@
+serverurl='php/'
+produkter=[{"id":1, "namn":"Smör","checked":0},
+            {"id":2,"namn":"Korv","checked":0},
+            {"id":3,"namn":"Bröd","checked":0}];
+
+window.onload = function() {
+    getProducts();
+
+}
+
+function getProducts() {
+    //radera vid fungerande php
+    appendProducts(produkter);
+
+
+    fetch(serverurl+'hamtaAlla/')
+        .then(function(response) {
+            if(response.status==200) {
+                return response.json();
+            }
+        })
+        .then(function(data) {
+            appendProducts(data);
+        })
+}
+
+function appendProducts(data){
+    console.log(data);
+    tabell=document.getElementById("varatable");
+    tabell.innerHTML="";
+
+    for(let i=0;i<data.length;i++) {
+        let rad=document.createElement("tr");
+
+        let checkboxtd=document.createElement("td");
+        let checkbox=document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.onclick=function(){
+            checkProduct(data.id);
+        }
+        checkboxtd.appendChild(checkbox);
+
+        let texttd=document.createElement("td");
+        texttd.innerHTML=data[i].namn;
+
+        let redigeratd=document.createElement("td");
+        let redigeraicon=document.createElement("i");
+        redigeraicon.classList.add("material-icons");
+        redigeraicon.innerHTML="edit";
+        redigeraicon.onclick=function(){
+            editProduct(data[i].id)
+        }
+        redigeratd.appendChild(redigeraicon);
+        
+        let raderatd=document.createElement("td");
+        let raderaicon=document.createElement("i");
+        raderaicon.classList.add("material-icons");
+        raderaicon.innerHTML="delete";
+        raderaicon.onclick=function(){
+            deleteProduct(data[i].id)
+        }
+        raderatd.appendChild(raderaicon);
+
+        rad.appendChild(checkboxtd);
+        rad.appendChild(texttd);
+        rad.appendChild(redigeratd);
+        rad.appendChild(raderatd);
+        tabell.appendChild(rad);
+    }
+
+}
+
+
+/*
+<tr>
+    <td><input type="checkbox"></td>
+    <td>Frukt</td>
+    <td><i class="material-icons">edit</i></td>
+    <td><i class="material-icons">delete</i></td>
+</tr>
+*/
--- php/uppdateraVara.php
+++ php/uppdateraVara.php
@@ -1,2 +1,54 @@
 <?php
 declare(strict_types=1);
+require_once "funktioner.php";
+
+//Kontrollera anropsmetod
+if ($_SERVER['REQUEST_METHOD']!=='POST') {
+    $error=new stdClass();
+    $error->meddelande=["Wrong method", "Sidan ska anropas med POST"];
+    skickaJSON($error, 405);
+}
+
+//Kontrollera indata
+$id=filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+$vara=filter_input(INPUT_POST, 'vara', FILTER_SANITIZE_SPECIAL_CHARS);
+
+//Skapa ett objekt för felmeddelanden
+$error=new stdClass();
+$error->meddelande = [];
+if(!isset($id) || $id===false || $id<1) {
+    //Felaktigt id, lägg till meddelande till
+    $error->meddelande[]="'id' saknas eller är felaktigt";
+}
+
+if(isset($vara) || mb_strlen($vara)>50) {
+    //Felaktig vara, lägg till meddelande till felobjekt
+    $error->meddelande[]="'vara' saknas eller är för långt";
+}
+
+//Har felobjektet några meddelanden
+if(count($error->meddelande)>0) {
+    //Lägg till ett generellt meddelande först i arrayen
+    array_unshift($error->meddelande, "Bad request");
+    skickaJSON($error, 400);
+}
+
+//Koppla databas
+$db=connectDB();
+
+//Uppdatera tabellen
+$sql="UPDATE varor SET namn=:vara WHERE id=:id";
+$stmt=$db->prepare($sql);
+
+$stmt->execute(['id' => $id, 'vara' => $vara]);
+
+//Returnera svar
+if($stmt->rowCount()>0) {
+    $out=new stdClass();
+    $out->meddelande="OK";
+    skickaJSON($out);
+}else {
+    $error=new stdClass();
+    $error->meddelande=["Okänt fel", "Kunde inte uppdatera vara"];
+    skickaJSON($error, 500);
+}
