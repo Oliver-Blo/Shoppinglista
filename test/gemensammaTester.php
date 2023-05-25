043b11bae069133bc3da0662d7b61a4d29d14fd5
DESKTOP-EH9DT1H\Oliver <47489@gymnasium.ax>
2023-05-25 09:39:14 +0300

    Testa kryssa vara fungerar

--- test/gemensammaTester.php
+++ test/gemensammaTester.php
@@ -29,3 +29,14 @@ function raderaVara(int $id):void {
     $db=connectDB();
     $db->exec("DELETE FROM varor WHERE id=$id");
 }
+
+function lasVara(int $id):stdClass {
+    $db=connectDB();
+    $row=$db->query("SELECT * FROM varor WHERE id=$id");
+    return $row->fetchObject();
+}
+
+function kryssaVara(int $id):void {
+    $db=connectDB();
+    $db->query("UPDATE varor SET checked=1 WHERE id=$id");
+}
--- test/testKryssaVara.php
+++ test/testKryssaVara.php
@@ -31,10 +31,12 @@ try {
     idFinnsInte($ch);
 
     //OK - sätt kryss
-
+    echo "<p class='info'>Test OK - sätt kryss</p>";
+    sattKryss($ch);
 
     //OK - ta bort kryss
-
+    echo "<p class='info'>Test OK - ta bort kryss</p>";
+    tabortKryss($ch);
 
 }catch (Exception $e) {
     echo "<p class='error'>";
@@ -45,10 +47,78 @@ try {
     curl_close($ch);
 }
 
+function tabortKryss($curlHandle) {
+    //Koppla mot databas
+    $db=connectDB();
+
+    //Lägg till vara
+    $id=skapaVara('test');
+
+    //Sätt kryss och kontrollera att det är satt
+    kryssaVara($id);
+    $vara=lasVara($id);
+    if(!$vara->checked){
+        echo "<p class='error'>Kunde inte sätta kryss, avbryter testet</p>";
+        raderaVara($id);
+    }
+
+    //Anropa sidan
+    curl_setopt($curlHandle, CURLOPT_POST, true);
+    $data=['id' => $id];
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $data);
+    $jsonSvar=curl_exec($curlHandle);
+    $status=curl_getinfo($curlHandle, CURLINFO_RESPONSE_CODE);
+
+    ///Skriv ut svaret
+    if($status===200) {
+        echo "<p class='ok'>Kryssa av vara fungerade</p>";
+    }else {
+        echo "<p class='error'>Kryssa av vara fungerade inte, status=$status istället för 200</p>";
+    }
+
+    //Radera vara
+    raderaVara($id);
+}
+
+function sattKryss($curlHandle) {
+    //Koppla mot databas
+    $db=connectDB();
+
+    //Skapa post
+    $id=skapaVara('test');
+
+    //Kontrollera att kryss är tomt - läs vara och kontrollera svaret
+    $vara=lasVara($id);
+    if($vara->checked) {
+        echo "<p class='error'>Kunde inte skapa vara utan kryss</p>";
+        raderaVara($id);
+        return;
+    }
+
+    //Sätt kryss - anropa sidan med POST och rätt data
+    curl_setopt($curlHandle, CURLOPT_POST, true);
+    $data=['id'=>$id];
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $data);
+
+    $jsonSvar=curl_exec($curlHandle);
+    $status=curl_getinfo($curlHandle, CURLINFO_RESPONSE_CODE);
+
+    //Skriv ut resultat
+    if($status===200) {
+        echo "<p class='ok'>Kryssa vara fungerade</p>";
+    } else {
+        echo "<p class='error'>Kryssa vara fungerade inte, status=$status istället för 200</p>";
+    }
+
+    //Radera post
+    raderaVara($id);
+
+}
+
 function idFinnsInte($curlHandle) {
     //Koppla mot databas och starta transaktion
     $db=connectDB();
-    $db->beginTransaction();
+
 
     //Skapa en ny post
     $id=skapaVara("test");
@@ -73,9 +143,6 @@ function idFinnsInte($curlHandle) {
     } else {
         echo "<p class='error'>Fick status=status istället för förväntat 400</p>";
     }
-
-    //Rulla tillbaka alla transaktioner
-    $db->rollBack();
 }
 
 
