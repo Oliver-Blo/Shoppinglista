f36aef0c6061538f71ce5e2b1ce4fb88e6e5700e
DESKTOP-EH9DT1H\Oliver <47489@gymnasium.ax>
2023-05-30 19:50:10 +0300

    Lagt till kommentarer

--- Shoppinglista.js
+++ Shoppinglista.js
@@ -1,6 +1,7 @@
 serverurl='php/'
 produkter=[];
 
+//När fönstret laddar görs funktionerna saveProduct, deleteAllProducts, och deleteCheckedProducts
 window.onload = function() {
     getProducts();
     document.getElementById("varabutton").onclick=function() {
@@ -14,37 +15,42 @@ document.getElementById("raderavalda").onclick=function(){
 }
 }
 
-
+//Hämtar alla produkter från databasen
 function getProducts() {
-    //radera vid fungerande php
-    appendProducts(produkter);
 
-
+    //hämtar data från serverURLen med "hamtaAlla/" som slutet
     fetch(serverurl+'hamtaAlla/')
+        //Returnerar svaret som JSON om statusen är 200
         .then(function(response) {
             if(response.status==200) {
                 return response.json();
             }
         })
+        //När den gjort klart det förra steget kallar den på appendProducts funktionen
         .then(function(data) {
             appendProducts(data);
         })
 }
 
+//Skapar raderna och lägger till edit och delete ikonerna
 function appendProducts(data){
     console.log(data);
+    //Gör "varatable" tom
     tabell=document.getElementById("varatable");
     tabell.innerHTML="";
-
+    
+    //Gör en loop som fortsätter tills i är lika långt som data.length
     for(let i=0;i<data.length;i++) {
+        //Sätter namn på variablerna "tr", "td" och "input"
         let rad=document.createElement("tr");
-
         let checkboxtd=document.createElement("td");
         let checkbox=document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
+        //Om data är checked sätter den checked som 1
         if(data[i].checked){
             checkbox.checked=1;
         }
+        //När man clickar på checkboxen gör den funktionen checkProduct
         checkbox.onclick=function(){
             checkProduct(data[i].id);
         }
@@ -54,16 +60,17 @@ function appendProducts(data){
         texttd.id="vara"+data[i].id;
         texttd.innerHTML=data[i].namn;
 
+        //Skapar redigera ikonen brevid produkterna och använder "editVaraForm" funktionen då man clickar på den
         let redigeratd = document.createElement("td");
-let redigeraicon = document.createElement("i");
-redigeraicon.classList.add("material-icons");
-redigeraicon.innerHTML = "edit";
-redigeraicon.addEventListener("click", function() {
+        let redigeraicon = document.createElement("i");
+        redigeraicon.classList.add("material-icons");
+        redigeraicon.innerHTML = "edit";
+        redigeraicon.addEventListener("click", function() {
     editVaraForm(data[i].id);
 });
 redigeratd.appendChild(redigeraicon);
 
-        
+        //Skapar radera ikonen brevid produkterna och använder "deleteProduct" funktionen då man clickar på den
         let raderatd=document.createElement("td");
         let raderaicon=document.createElement("i");
         raderaicon.classList.add("material-icons");
@@ -73,14 +80,15 @@ redigeratd.appendChild(redigeraicon);
         }
         raderatd.appendChild(raderaicon);
 
+        //Lägger till checkbox, text, redigera ikonen och radera ikonen i raden
         rad.appendChild(checkboxtd);
         rad.appendChild(texttd);
         rad.appendChild(redigeratd);
         rad.appendChild(raderatd);
         tabell.appendChild(rad);
     }
-
 }
+
 
 
 
--- varor.js
+++ varor.js
@@ -1,137 +1,170 @@
+//Skapar en ny vara när man trycker på Lägg till knappen på sidan, men endast om den inte är tom
 function saveProduct() {
+    //Hämtar det som står i "varainput" fältet
     let varanode = document.getElementById('varainput');
     let vara = varanode.value;
 
+    //Om fältet inte är tomt gör den resten av det som står
     if (vara.trim() != "") {
         let FD = new FormData();
         FD.append("vara", vara);
 
+        //fetchar sparaVara som POST
         fetch(serverurl + 'sparaVara.php', {
             method: 'POST',
             body: FD
         })
+        //Om den svarar med status 200 returnerar den svaret som JSON
         .then(function(response) {
             if (response.status == 200) {
                 return response.json();
             }
         })
+        //När det förra steget är klart uppdaterar den produkterna
         .then(function(data) {
             getProducts();
         });
     }
+    //Ändrar tillbaka fältet som tomt
     varanode.value = ""; 
 }
 
+//När man trycker på edit knappen så far varan man valt till ett fält för att man ska kunna ändra på det
 function editVaraForm(id) {
+    //Skickar varan till fältet
     document.getElementById("varainput").value = document.getElementById("vara" + id).innerHTML;
     document.getElementById("varabutton").onclick = function() {
         editProduct(id);
     };
+    //Ändrar "Lägg till" knappen till "Spara" knapp
     document.getElementById("varabutton").innerHTML = "Spara";
 }
 
+//När man trycker på spara knappen efter att man har ändrat på en vara kommer den även ändras i listan och databasen
 function editProduct(id) {
+    //Hämtar det som står i "varainput" och sätter det i variabeln "vara"
     let varanode = document.getElementById('varainput');
     let vara = varanode.value;
 
+    //Om fältet inte är tomt gör den resten av det som står
     if (vara.trim() != "") {
         let FD = new FormData();
         FD.append("vara", vara);
         FD.append("id", id);
 
+        //Fetchar uppdateraVara som POST
         fetch(serverurl + 'uppdateraVara.php', {
             method: 'POST',
             body: FD
         })
+        //När det är klart returnerar den ett svar som JSON om statuset är 200
         .then(function(response) {
             if (response.status == 200) {
                 return response.json();
             }
         })
+        //Om det förra lyckades uppdaterar det produktlistan
         .then(function(data) {
             getProducts();
         });
-
+        //Ändrar tillbaka fältet som tomt
         varanode.value = "";
     }
-    
+    //Trycker man på "Spara" knappen görs funktionen "saveProduct"
     document.getElementById("varabutton").onclick = function() {
         saveProduct();
     };
+    //Ändrar tillbaka knappen till "Lägg till" från "Spara"
     document.getElementById("varabutton").innerHTML = "Lägg till";
 }
 
+//Raderar en produkt efter att man har svarat ja på en "confirm" fråga som kommer fram då man trycker på soptunnan
 function deleteProduct(id, product) {
 
-        if(confirm("Vill du radera "+product.namn+"?")){
-        let FD = new FormData();
-        FD.append("id", id);
-
-        fetch(serverurl + 'raderaVara.php', {
-            method: 'POST',
-            body: FD
-        })
-        .then(function(response) {
-            if (response.status == 200) {
-                return response.json();
-            }
-        })
-        .then(function(data) {
-            getProducts();
-        });
-}}
-
-
-function deleteAllProducts() {
-
-    if(confirm("Vill du radera alla varor?")){
-
-    fetch(serverurl + 'raderaAllaVaror.php', {
-        method: 'POST'
+    //Om man svarar ja på confirmen gör den resten av det som står
+    if(confirm("Vill du radera "+product.namn+"?")){
+    let FD = new FormData();
+    FD.append("id", id);
+    
+    //Fetchar raderaVara som POST
+    fetch(serverurl + 'raderaVara.php', {
+        method: 'POST',
+        body: FD
     })
+    //Returnerar svaret som JSON om statuset är 200
     .then(function(response) {
         if (response.status == 200) {
             return response.json();
         }
     })
+    //Om det förra lyckades uppdaterar det produktlistan
     .then(function(data) {
         getProducts();
     });
 }}
 
+//Raderar alla produkter efter att man har svart ja på en "confirm" fråga som kommer fram då man trycker på Radera alla knappen
+function deleteAllProducts() {
 
+    //Om man svarar ja på confirmen gör den resten av det som står
+    if(confirm("Vill du radera alla varor?")){
+
+    //Fetchar raderaAllaVaror som POST
+        fetch(serverurl + 'raderaAllaVaror.php', {
+            method: 'POST'
+        })
+        //Returnerar svaret som JSON om statuset är 200
+        .then(function(response) {
+            if (response.status == 200) {
+                return response.json();
+            }
+        })
+        //Om det förra lyckades uppdaterar det produktlistan
+        .then(function(data) {
+            getProducts();
+    });
+}}
+
+//Kryssar för en vara då man trycker på checkboxen brevid
 function checkProduct(id) {
-
     let FD = new FormData();
     FD.append("id", id);
 
+    //fetchar kryssaVara som POST
     fetch(serverurl + 'kryssaVara.php', {
         method: 'POST',
         body: FD
     })
+    //Returnerar svaret som JSON om statuset är 200
     .then(function(response) {
         if (response.status == 200) {
             return response.json();
         }
     })
+    //Om det förra lyckades uppdaterar det produktlistan
     .then(function(data) {
         getProducts();
     });
 }
 
+//Raderar alla ikryssade produkter efter att man svarar ja på en "confirm" fråga som kommer upp då man trycker på Radera valda
 function deleteCheckedProducts() {
 
+    //Om man svarar på confirmen gör den resten av det som står
     if(confirm("Vill du radera valda varor?")){
-
-    fetch(serverurl + 'raderaValda.php', {
-        method: 'POST'
-    })
-    .then(function(response) {
-        if (response.status == 200) {
-            return response.json();
-        }
-    })
-    .then(function(data) {
-        getProducts();
-    });
+    
+        //fetchar raderaValda som POST
+        fetch(serverurl + 'raderaValda.php', {
+            method: 'POST'
+        })
+        //Returnerar svaret som JSON om statuset är 200
+        .then(function(response) {
+            if (response.status == 200) {
+                return response.json();
+            }
+        })
+        //Om det förra lyckades uppdaterar det produktlistan
+        .then(function(data) {
+            getProducts();
+        });
 }}
